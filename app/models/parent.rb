d2ed88f6e45d6eae4a7bcd8879de5d915a78cb31
Dakota <kramerdakota@gmail.com>
2012-02-13 18:03:07 -0600

    done with 2.3.2

--- app/models/parent.rb
+++ app/models/parent.rb
@@ -1,2 +1,6 @@
 class Parent < ActiveRecord::Base
+    validates :name, :length => { :maximum => 30 },
+                     :presence => true
+    validates :gender, :inclusion => { :in => %w( male female) },
+                       :presence => true
 end
